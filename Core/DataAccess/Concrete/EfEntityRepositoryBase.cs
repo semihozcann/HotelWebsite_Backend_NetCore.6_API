f3afaf4f6215e88ff2d5a32427b91730b8c06ddd
semih özcan <semihozcann@outlook.com>
2023-01-30 20:20:07 +0300

    thirdComit

---
+++ Core/DataAccess/Concrete/EfEntityRepositoryBase.cs
@@ -0,0 +1,83 @@
+﻿using Core.DataAccess.Abstract;
+using Core.Entities.Abstract;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess.Concrete
+{
+    public class EfEntityRepositoryBase<TEntity, TContext> : IEntityRepository<TEntity>
+        where TEntity : class, IEntity, new()
+        where TContext : DbContext, new()
+    {
+        #region Add
+
+        public void Add(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var addedEntity = context.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();
+            }
+        }
+
+        #endregion
+
+        #region Delete
+
+        public void Delete(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+        #endregion
+
+        #region Get
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            using (TContext context = new TContext())
+            {
+                return context.Set<TEntity>().SingleOrDefault(filter);
+            }
+        }
+
+        #endregion
+
+        #region GetAll
+
+        public List<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null)
+        {
+            using (TContext context = new TContext())
+            {
+                return filter == null ? context.Set<TEntity>().ToList() : context.Set<TEntity>().Where(filter).ToList();
+            }
+        }
+
+        #endregion
+
+        #region Update
+
+        public void Update(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var updatedEntity = context.Entry(entity);
+                updatedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/Services/EfUserDal.cs
@@ -0,0 +1,16 @@
+﻿using Core.DataAccess.Concrete;
+using Core.Entities.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework.Services
+{
+    public class EfUserDal : EfEntityRepositoryBase<User, HotelDbContext>, IUserDal
+    {
+    }
+}
